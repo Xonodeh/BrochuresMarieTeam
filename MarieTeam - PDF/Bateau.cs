86882b81169bde17f4fdf9ff1d1fc28368f185f8
Nael <haddadi.nael@gmail.com>
2025-04-15 12:04:33 +0200

    Ajoutez des fichiers projet.

---
+++ MarieTeam - PDF/Bateau.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarieTeam___PDF
+{
+    public class Bateau
+    {
+        private string idBat;
+        private string nomBat;
+        private double longueurBat;
+        private double largeurBat;
+
+        public Bateau(string unId, string unNom, double uneLongueur, double uneLargeur)
+        {
+            idBat = unId;
+            nomBat = unNom;
+            longueurBat = uneLongueur;
+            largeurBat = uneLargeur;
+        }
+
+        public override string ToString()
+        {
+            return $"Nom du bateau : {nomBat}\nLongueur : {longueurBat} mètres\nLargeur : {largeurBat} mètres";
+        }
+
+        public string IdBat => idBat;
+        public string NomBat => nomBat;
+        public double LongueurBat => longueurBat;
+
+        public double LargeurBat => largeurBat;
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
