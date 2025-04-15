6c2cca1b01e3d457e1b9913a044025e3f5162bd4
Nael <haddadi.nael@gmail.com>
2025-04-15 14:40:19 +0200

    ajout class passerelle et jeuEnregistrement + connexion bdd

---
+++ MarieTeam - PDF/JeuEnregistrement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace MarieTeam___PDF
+{
+    public class JeuEnregistrement
+    {
+        private MySqlConnection connexion;
+        private MySqlCommand commande;
+        private MySqlDataReader lecteur;
+
+        public JeuEnregistrement(string requeteSQL)
+        {
+            string chaineConnexion = "server=localhost;uid=root;pwd=;database=marieteam;";
+            connexion = new MySqlConnection(chaineConnexion);
+            commande = new MySqlCommand(requeteSQL, connexion);
+
+            try
+            {
+                connexion.Open();
+                lecteur = commande.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur de connexion : " + ex.Message);
+            }
+        }
+
+        public void Suivant()
+        {
+            if (!lecteur.IsClosed) lecteur.Read();
+        }
+
+        public bool Fin()
+        {
+            return lecteur == null || !lecteur.HasRows || lecteur.IsClosed || lecteur.IsDBNull(0);
+        }
+
+        public object GetValeur(string nomChamp)
+        {
+            return lecteur[nomChamp];
+        }
+
+        public void Fermer()
+        {
+            if (lecteur != null && !lecteur.IsClosed) lecteur.Close();
+            if (connexion.State == ConnectionState.Open) connexion.Close();
+        }
+    }
+}
---
+++ MarieTeam - PDF/Passerelle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarieTeam___PDF
+{
+    namespace MarieTeam___PDF
+    {
+        public static class Passerelle
+        {
+            public static List<Equipement> ChargerLesEquipements(string idBateau)
+            {
+                List<Equipement> listeEquipements = new List<Equipement>();
+
+                string requete = $"SELECT e.idEquip, e.libEquip FROM equipement e " +
+                                 $"JOIN disposer d ON d.idEquip = e.idEquip " +
+                                 $"WHERE d.idBat = '{idBateau}'";
+
+                JeuEnregistrement jeu = new JeuEnregistrement(requete);
+
+                while (!jeu.Fin())
+                {
+                    string idEquip = jeu.GetValeur("idEquip").ToString();
+                    string libEquip = jeu.GetValeur("libEquip").ToString();
+
+                    Equipement e = new Equipement(idEquip, libEquip);
+                    listeEquipements.Add(e);
+
+                    jeu.Suivant();
+                }
+
+                jeu.Fermer();
+                return listeEquipements;
+            }
+
+            public static List<BateauVoyageur> ChargerLesBateauxVoyageurs()
+            {
+                List<BateauVoyageur> liste = new List<BateauVoyageur>();
+
+                string requete = "SELECT * FROM bateauvoyageur";
+
+                JeuEnregistrement jeu = new JeuEnregistrement(requete);
+
+                while (!jeu.Fin())
+                {
+                    string id = jeu.GetValeur("idBat").ToString();
+                    string nom = jeu.GetValeur("nomBat").ToString();
+                    double longueur = Convert.ToDouble(jeu.GetValeur("longueurBat"));
+                    double largeur = Convert.ToDouble(jeu.GetValeur("largeurBat"));
+                    double vitesse = Convert.ToDouble(jeu.GetValeur("vitesseBatVoy"));
+                    string image = jeu.GetValeur("imageBatVoy").ToString();
+
+                    List<Equipement> equipements = ChargerLesEquipements(id);
+
+                    BateauVoyageur b = new BateauVoyageur(id, nom, longueur, largeur, vitesse, image, equipements);
+                    liste.Add(b);
+
+                    jeu.Suivant();
+                }
+
+                jeu.Fermer();
+                return liste;
+            }
+        }
+    }
+}
